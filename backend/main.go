f4273e3434701d345f3f470099065eff839a896b
daichi2mori <daichi2mori@gmail.com>
2024-08-29 23:32:24 +0900

    feat: add http method

--- backend/main.go
+++ backend/main.go
@@ -3,8 +3,11 @@ package main
 import (
 	"database/sql"
 	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
+
+	_ "github.com/mattn/go-sqlite3"
 )
 
 type Todo struct {
@@ -15,28 +18,48 @@ type Todo struct {
 
 var db *sql.DB
 
-func init() {
-	var err error
-	db, err = sql.Open("sqlite3", "todos.db")
-	if err != nil {
-		log.Fatal(err)
-	}
+// func init() {
+// 	var err error
+// 	db, err = sql.Open("sqlite3", "todos.db")
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
 
-	createTable := `
-	CREATE TABLE IF NOT EXISTS todos (
-		id INTEGER PRIMARY KEY AUTOINCREMENT,
-		content TEXT,
-		Completed BOOLEAN
-	);`
+// 	createTable := `
+// 	CREATE TABLE IF NOT EXISTS todos (
+// 		id INTEGER PRIMARY KEY AUTOINCREMENT,
+// 		content TEXT,
+// 		Completed BOOLEAN
+// 	);`
 
-	_, err = db.Exec(createTable)
+// 	_, err = db.Exec(createTable)
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
+// }
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("GET /todo", test)
+	mux.HandleFunc("POST /todo", func(w http.ResponseWriter, r *http.Request) {
+		json.NewEncoder(w).Encode(map[string]string{"message": "POST"})
+	})
+	mux.HandleFunc("PUT /todo", func(w http.ResponseWriter, r *http.Request) {
+		json.NewEncoder(w).Encode(map[string]string{"message": "PUT"})
+	})
+	mux.HandleFunc("DELETE /todo", func(w http.ResponseWriter, r *http.Request) {
+		json.NewEncoder(w).Encode(map[string]string{"message": "DELETE"})
+	})
+
+	fmt.Println("Server is running on port 8080...")
+	err := http.ListenAndServe(":8080", mux)
 	if err != nil {
-		log.Fatal(err)
+		log.Fatal("ListenAndServe: ", err)
 	}
 }
 
-func main() {
-
+func test(w http.ResponseWriter, r *http.Request) {
+	json.NewEncoder(w).Encode(map[string]string{"message": "GET"})
 }
 
 func getTodo(w http.ResponseWriter, r *http.Request) {
@@ -61,6 +84,7 @@ func getTodo(w http.ResponseWriter, r *http.Request) {
 
 	// wのヘッダーにcontent-typeを追加
 	w.Header().Set("Content-Type", "application/json")
-	//
+	// NewEncoder()の引数内にエンコーダーを作成
+	// Encode()の引数を実際にエンコードする
 	json.NewEncoder(w).Encode(todos)
 }
